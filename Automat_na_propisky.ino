823cea136cc2e9119ad2a52c7403f0c32d444e5b
masterpajas <94288953+masterpajas@users.noreply.github.com>
2026-01-03 23:46:35 +0100

    dispensing timeout, service key, pin initializations

--- Automat_na_propisky.ino
+++ Automat_na_propisky.ino
@@ -6,23 +6,27 @@
 #include <ESP32QRCodeReader.h>
 
 //indicator pins
-#define POWER 4
-#define WIFI 16
+#define POWER 1
+#define WIFI 3
 #define EMPTY 0
 
 #define IR 2
+#define IR_THRESHOLD 3000
 
 //pins for stepper
-#define STEPPER_A 14
-#define STEPPER_B 15
-#define STEPPER_C 13
-#define STEPPER_D 12
+#define STEPPER_A 4
+#define STEPPER_B 2
+#define STEPPER_C 14
+#define STEPPER_D 15
 
 #define STEPPER_DELAY 1500
 
 const char* host = "https://pgdtypgzzdchqefcgcaz.supabase.co";
 const int httpsPort = 443;
-const char* supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBnZHR5cGd6emRjaHFlZmNnY2F6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTg1MjAxNzEsImV4cCI6MjA3NDA5NjE3MX0.a0VHx01HJRa6G3MGTzR3pLHRjNr1cUiaiENOwWicizc";
+const char* supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBnZHR5cGd6emRjaHFlZmNnY2F6Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc1ODUyMDE3MSwiZXhwIjoyMDc0MDk2MTcxfQ.GbV66aF7zwqLtsqrZ-gGKScqnaV-d3lTO7WDMBLKBY8";
+
+ESP32QRCodeReader reader(CAMERA_MODEL_AI_THINKER);
+struct QRCodeData qrCodeData;
 
 //new credentials
 bool connectWiFi(String ssid, String password) {
@@ -47,6 +51,7 @@ bool connectWiFi(String ssid, String password) {
   preferences.putString("ssid", ssid);
   preferences.putString("password", password);
 
+  digitalWrite(WIFI, HIGH);
   return true;
 }
 
@@ -79,86 +84,89 @@ bool connectWiFi() {
     }
   }
   
+  digitalWrite(WIFI, HIGH);
   return true;
 }
 
-void stepperRotate()
-{
+void stepperRotate() {
   //krok 1
   digitalWrite(STEPPER_A, HIGH); 
   digitalWrite(STEPPER_B, LOW); 
   digitalWrite(STEPPER_C, LOW); 
   digitalWrite(STEPPER_D, LOW); 
-  delay(rychlost);
+  delay(1);
 
   //krok 2
   digitalWrite(STEPPER_A, HIGH); 
   digitalWrite(STEPPER_B, HIGH); 
   digitalWrite(STEPPER_C, LOW); 
   digitalWrite(STEPPER_D, LOW); 
-  delay(rychlost);
+  delay(1);
 
   //krok 3
   digitalWrite(STEPPER_A, LOW); 
   digitalWrite(STEPPER_B, HIGH); 
   digitalWrite(STEPPER_C, LOW); 
   digitalWrite(STEPPER_D, LOW); 
-  delay(rychlost);
+  delay(1);
 
   //krok 4
   digitalWrite(STEPPER_A, LOW); 
   digitalWrite(STEPPER_B, HIGH); 
   digitalWrite(STEPPER_C, HIGH); 
   digitalWrite(STEPPER_D, LOW); 
-  delay(rychlost);
+  delay(1);
 
   //krok 5
   digitalWrite(STEPPER_A, LOW); 
   digitalWrite(STEPPER_B, LOW);
   digitalWrite(STEPPER_C, HIGH); 
   digitalWrite(STEPPER_D, LOW); 
-  delay(rychlost);
+  delay(1);
 
   //krok 6
   digitalWrite(STEPPER_A, LOW); 
   digitalWrite(STEPPER_B, LOW);
   digitalWrite(STEPPER_C, HIGH);
   digitalWrite(STEPPER_D, HIGH); 
-  delay(rychlost);
+  delay(1);
 
   //krok 7
   digitalWrite(STEPPER_A, LOW); 
   digitalWrite(STEPPER_B, LOW); 
   digitalWrite(STEPPER_C, LOW); 
   digitalWrite(STEPPER_D, HIGH); 
-  delay(rychlost);
+  delay(1);
 
   //krok 8
   digitalWrite(STEPPER_A, HIGH); 
   digitalWrite(STEPPER_B, LOW); 
   digitalWrite(STEPPER_C, LOW); 
   digitalWrite(STEPPER_D, HIGH); 
-  delay(rychlost);
+  delay(1);
 }
 
 bool deductToken(char* uid, char* temporary_key) {
   HTTPClient http;
-
-  // Target URL for the "users" table
-  String url = "https://pgdtypgzzdchqefcgcaz.supabase.co/rest/v1/rpc/subtract_token" + uid;
-
-  // Prepare JSON body – e.g. set tokens to 4
-  String body = "{\"token_count\":4}";
-
-  http.begin(url);
+  http.begin("https://pgdtypgzzdchqefcgcaz.supabase.co/rest/v1/rpc/subtract_token");
   http.addHeader("apikey", supabaseKey);
-  //http.addHeader("Authorization", String("Bearer ") + supabaseKey);
-  http.addHeader("Content-Type", "application/json");  
-  http.addHeader("Prefer", "count=exact");
+  http.addHeader("Authorization", String("Bearer ") + supabaseKey);
   
-  int httpResponseCode = http.PATCH(body);
+  char* body = "{ \"user_id\":12 }";
+  
+  int httpResponseCode = http.POST(body);
 
-  if (httpResponseCode > 0) {
+  if (httpResponseCode > 0)
+  {
+    if (http.getString() == "true")
+      return true;
+    else
+      return false;
+  }
+
+  http.end();
+
+  /*if (httpResponseCode > 0) {
     Serial.println("Response: " + http.getString());
     http.end();
     return true;
@@ -167,17 +175,10 @@ bool deductToken(char* uid, char* temporary_key) {
     Serial.printf("Error: %s\n", http.errorToString(httpResponseCode).c_str());
     http.end();
     return false;
-  }
+  }*/
 }
 
-char* scanQR()
-{
-  ESP32QRCodeReader reader(CAMERA_MODEL_AI_THINKER);
-  struct QRCodeData qrCodeData;
-
-  reader.setup();
-  reader.begin();
-
+char* scanQR() {
   while (true) 
   {
     if (reader.receiveQrCode(&qrCodeData, 100) && qrCodeData.valid) 
@@ -190,8 +191,35 @@ char* scanQR()
   }
 }
 
+void blinkLED(int pin) {
+  for (int i = 0; i < 3; i++)
+  {
+    digitalWrite(pin, LOW);
+    delay(500);
+    digitalWrite(pin, HIGH);
+    delay(500);
+  }
+}
+
 void setup() {
+  pinMode(POWER, OUTPUT);
+  pinMode(WIFI, OUTPUT);
+  pinMode(EMPTY, OUTPUT);
+  pinMode(IR, INPUT);
+
+  pinMode(STEPPER_A, OUTPUT);
+  pinMode(STEPPER_B, OUTPUT);
+  pinMode(STEPPER_C, OUTPUT);
+  pinMode(STEPPER_D, OUTPUT);
+
+  digitalWrite(POWER, HIGH);
+  digitalWrite(WIFI, LOW);
+  digitalWrite(EMPTY, LOW);
+
   Serial.begin(115200);
+
+  reader.setup();
+  reader.begin();
 
   if (connectWiFi())
   {
@@ -244,9 +272,10 @@ void setup() {
 }
 
 void loop() {
+  Serial.println("Scan QR");
   char* data = scanQR();
   char* pch = strtok(data, "/");
-  int uid;
+  char* uid;
   char* tempKey;
 
   if (pch != NULL)
@@ -257,7 +286,7 @@ void loop() {
   }
   else 
   {
-    Serial.println("Invalid QR data");
+    Serial.println("Invalid user QR data");
   }
 
   if (pch != NULL)
@@ -267,23 +296,31 @@ void loop() {
   }
   else 
   {
-    Serial.println("Invalid QR data");
+    Serial.println("Invalid user QR data");
   }
 
   if(deductToken(uid, tempKey))
   {
-    Serial.println("Dispensing now...")
+    Serial.println("Dispensing now...");
 
-    bool dispensed = false;
-    while (!dispensed)
+    unsigned long timeout = millis() + 10000;
+    while (true)
     {
       stepperRotate();
-      // continuous sensing
-      // if sensor triggers, dispensed = true;
+      
+      if (analogRead(IR) > IR_THRESHOLD)
+        break;
+
+      if (millis() >= timeout)
+      {
+        Serial.println("Error while dispensing");
+        break;
+      }
     }
   }
   else
   {
-    //indicate problems
+    blinkLED(POWER);
+    digitalWrite(POWER, HIGH);
   }
 }
